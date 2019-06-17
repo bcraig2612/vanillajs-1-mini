77b25715097ff5b286221e4bebb1a561217b9cc4
Brandon Caig <bcraig2612@gmail.com>
2019-06-17 12:28:10 -0700

    Completed.VanillaJS.Mini.Project

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-console.log("hello world");
+// console.log("hello world");
+// console.log(window);
+
+
+// ******************** EXAMPLE 0: ********************
+// let numArr = [1, 2, 3, 4, 5];
+// for (let i = 0; i <numArr.length; i++) {
+//   console.log('example0', numArr[i]);
+// }
+
+
+// ******************** EXAMPLE 1: ********************
+// let mappedArr = numArr.map(function(num) {
+//   console.log('example1', num);
+// })
+
+
+// ******************** EXAMPLE 2: ********************
+// let numArr = [1, 2, 3, 4, 5];
+// for (let num in numArr) {
+//   console.log('example2', numArr[num]);
+// }
+
+
+// ******************** Implementing 1 of the EXAMPLES: ********************
+// function elementChecker(arr) {
+//   for (let num in arr) {
+//     console.log('example2', arr[num])
+//   }
+// }
+// elementChecker(numArr);
+
+// ==============================================================================================================================================
+
 const element = document.getElementById("counter");
+
 let count = 0;
 
 function increase() {
@@ -21,9 +55,13 @@ function reset() {
 }
 
 function selectTheme(theme) {
-  document.getElementsByTagName("body")[0].className = theme;
-  document.getElementsByTagName("main")[0].className = theme;
-  const buttons = document.getElementByTagName("button");
+ let body = document.getElementsByTagName("body");
+ body[0].className = theme;
+ 
+ let main = document.getElementsByTagName("main");
+ main[0].className = theme;
+ 
+ let buttons = document.getElementsByTagName("button");
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].className = theme;
   }
